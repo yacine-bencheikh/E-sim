749ce4c762a2fb2115c4d4a2389f2fa3f1c2415b
yassine ben cheikh <110359586+yacine-bencheikh@users.noreply.github.com>
2024-02-20 15:25:42 +0100

    Delete backEnd directory

--- backEnd/routers/route.reservation.js
+++
@@ -1,8 +0,0 @@
-const {makeReservation} = require("../controllers/controller.reservation")
-const express = require("express");
-
-const reservationRoute = express.Router();
-
-reservationRoute.post("/add",makeReservation)
-
-module.exports = reservationRoute;
